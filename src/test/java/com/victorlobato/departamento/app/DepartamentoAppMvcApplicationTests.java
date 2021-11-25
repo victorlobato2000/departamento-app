15cf2620c1df8fee66b61c767d1859fee776aa17
victorlobato2000 <victorsilva123190@gmail.com>
2021-11-25 06:29:36 -0300

    Adicionando dependências ao projeto

---
+++ src/test/java/com/victorlobato/departamento/app/DepartamentoAppMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorlobato.departamento.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartamentoAppMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
